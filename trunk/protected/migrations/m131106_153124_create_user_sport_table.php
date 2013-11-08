fa103e1101216c48623b933751477453c0478605
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2013-11-08 10:52:13 +0000

    Add composite primary keys (needs redo DB) fix path to yii framework

--- trunk/protected/migrations/m131106_153124_create_user_sport_table.php
+++ trunk/protected/migrations/m131106_153124_create_user_sport_table.php
@@ -6,7 +6,6 @@ class m131106_153124_create_user_sport_table extends CDbMigration
 	{
 		$this->createTable('tbl_user_sport', array(
 			// keys
-			'id' => 'pk',
 			'user_id' => 'integer NOT NULL REFERENCES tbl_user(id)',
 			'sport_id' => 'integer NOT NULL REFERENCES tbl_sport(id)',
 
@@ -16,6 +15,7 @@ class m131106_153124_create_user_sport_table extends CDbMigration
 			'updated_at' => 'timestamp',
 		));
 		
+		$this->addPrimaryKey('id', 'tbl_user_sport', 'user_id, sport_id');
 		$this->addForeignKey('sport_user_fk', 'tbl_user_sport', 'user_id', 'tbl_user', 'id');
 		$this->addForeignKey('user_sport_fk', 'tbl_user_sport', 'sport_id', 'tbl_sport', 'id');
 	}
