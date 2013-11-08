52363176b6f59db5d771b337cb0c9e339bba2cf2
Ondrej Danko <xjcook@users.noreply.github.com>
2013-11-08 10:52:13 +0000

    Add composite primary keys (needs redo DB) fix path to yii framework

--- protected/migrations/m131106_152406_create_user_course_table.php
+++ protected/migrations/m131106_152406_create_user_course_table.php
@@ -6,7 +6,6 @@ class m131106_152406_create_user_course_table extends CDbMigration
 	{
 		$this->createTable('tbl_user_course', array(
 			// keys
-			'id' => 'pk',
 			'user_id' => 'integer NOT NULL REFERENCES tbl_user(id)',
 			'course_id' => 'integer NOT NULL REFERENCES tbl_course(id)',
 
@@ -16,6 +15,7 @@ class m131106_152406_create_user_course_table extends CDbMigration
 			'updated_at' => 'timestamp',
 		));
 		
+		$this->addPrimaryKey('id', 'tbl_user_course', 'user_id, course_id');
 		$this->addForeignKey('course_user_fk', 'tbl_user_course', 'user_id', 'tbl_user', 'id');
 		$this->addForeignKey('user_course_fk', 'tbl_user_course', 'course_id', 'tbl_course', 'id');
 	}
--- protected/migrations/m131106_155513_create_student_sport_table.php
+++ protected/migrations/m131106_155513_create_student_sport_table.php
@@ -6,7 +6,6 @@ class m131106_155513_create_student_sport_table extends CDbMigration
 	{
 		$this->createTable('tbl_student_sport', array(
 			// keys
-			'id' => 'pk',
 			'student_id' => 'integer NOT NULL REFERENCES tbl_student(id)',
 			'sport_id' => 'integer NOT NULL REFERENCES tbl_sport(id)',
 
@@ -16,6 +15,7 @@ class m131106_155513_create_student_sport_table extends CDbMigration
 			'updated_at' => 'timestamp',
 		));
 		
+		$this->addPrimaryKey('id', 'tbl_student_sport', 'student_id, sport_id');
 		$this->addForeignKey('sport_student_fk', 'tbl_student_sport', 'student_id', 'tbl_student', 'id');
 		$this->addForeignKey('student_sport_fk', 'tbl_student_sport', 'sport_id', 'tbl_sport', 'id');
 	}
