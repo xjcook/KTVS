3b510dd3dac7a6c75299fef817f056dc9b07a02c
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2013-12-03 13:09:18 +0000

    Fix DB

--- trunk/protected/migrations/m131121_115823_add_sport_fk.php
+++ trunk/protected/migrations/m131121_115823_add_sport_fk.php
@@ -4,7 +4,10 @@ class m131121_115823_add_sport_fk extends CDbMigration
 {
 	public function safeUp()
 	{
-		//$this->addForeignKey('sport_schedule_fk', 'tbl_sport', 'schedule_id', 'tbl_schedule', 'id', 'NO ACTION', 'NO ACTION');
+		//$this->addForeignKey('sport_schedule_fk', 'tbl_sport', 'schedule_id', 'tbl_schedule', 'id', 'CASCADE', 'SET NULL');
+		
+		$this->insert('tbl_sport', array('name'=>'aerobik'));
+		$this->insert('tbl_sport', array('name'=>'hopsanie'));
 	}
 
 	public function safeDown()
