22680d6336f7532ad96f71e94862748380f5868d
Ondrej Danko <xjcook@users.noreply.github.com>
2013-12-03 11:08:29 +0000

    Add slovak translation

--- protected/views/news/index.php
+++ protected/views/news/index.php
@@ -3,7 +3,7 @@
 /* @var $dataProvider CActiveDataProvider */
 
 $this->breadcrumbs=array(
-	'News',
+	'Novinky',
 );
 
 $this->menu=array(
@@ -12,7 +12,7 @@ $this->menu=array(
 );
 ?>
 
-<h1>News</h1>
+<h1>Novinky</h1>
 
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'=>$dataProvider,
