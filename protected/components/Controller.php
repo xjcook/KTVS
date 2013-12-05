c7008233887e36eccc716f6982c6df7803459397
Ondrej Danko <xjcook@users.noreply.github.com>
2013-12-05 10:07:10 +0000

    Clean code

--- protected/components/Controller.php
+++ protected/components/Controller.php
@@ -20,35 +20,4 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
-	
-	/**
-	 * @var object model of LoginForm
-	 */
-	public $loginForm;
-	
-	/**
-	 * Overrides beforeAction() to set loginForm on every page
-	 * @see CController::beforeAction()
-	 */
-	protected function beforeAction($action) {
-		$this->loginForm=new LoginForm;
-	
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-	
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$this->loginForm->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($this->loginForm->validate() && $this->loginForm->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-	
-		return parent::beforeAction($action);
-	}
 }
