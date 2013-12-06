88599796c2b3c1fa239319a1bee8e3c8868529d1
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2013-12-06 11:49:05 +0000

    Clean SportMenu code

--- trunk/protected/components/SportMenu.php
+++ trunk/protected/components/SportMenu.php
@@ -4,16 +4,17 @@ class SportMenu extends CMenu
 {
     public function init()
     {
-        // Here we define query conditions.
         $criteria = new CDbCriteria;
         $criteria->order = '`id` ASC';
 
-        $sports = Sport::model()->findAll();
+        $sports = Sport::model()->findAll($criteria);
 
         foreach ($sports as $sport)
-            $this->items[] = array('label'=>$sport->name, 'url'=> Yii::app()->request->baseUrl . '/sport/' . $sport->id);
+        {
+            $this->items[] = array('label'=>$sport->name, 
+            	                   'url'=>Yii::app()->request->baseUrl.'/sport/'.$sport->id);
+        }
 
-        //$items = array(array('name'=>'aerobik'));
         parent::init();
     }
 }
