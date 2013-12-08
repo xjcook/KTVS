38f7b514a20139b9c285df5ec696f9a9d8a96459
Ondrej Danko <xjcook@users.noreply.github.com>
2013-12-08 20:34:25 +0000

    Fix bug in WebUser add comments

--- protected/components/WebUser.php
+++ protected/components/WebUser.php
@@ -5,22 +5,38 @@ class WebUser extends CWebUser {
   // Store model to not repeat query.
   private $_model;
  
-  // This is a function that checks the field 'role'
-  // in the User model to be equal to 1, that means it's admin
-  // access it by Yii::app()->user->isAdmin()
-  function isAdmin(){
+  /**
+   * Check if user is Admin
+   * field 'is_admin' must to be equal to 1, that means it's admin
+   * @return True if admin access
+   */
+  function isAdmin()
+  {
     $user = $this->loadUser(Yii::app()->user->id);
-    return intval($user->is_admin) == 1;
+    if ($user===null)
+        return 0;
+    else
+        return intval($user->is_admin) == 1;
   }
   
-  function checkUserAccess($model) {
-  	return $model->with(array(
-  		'users'=>array(
-  			'select'=>'users.id',
-  			'condition'=>'users.id=:id',
-  			'params'=>array(':id'=>Yii::app()->user->id),
-  		),
-  	))->exists();
+  /**
+   * Check if user has access
+   * @param CActiveRecord model with users relation
+   * @return True if granted access
+   */
+  function checkUserAccess($model) 
+  {
+	$userId = Yii::app()->user->id;
+	if ($userId===null)
+		return 0;
+	else
+		return $model->with(array(
+			'users'=>array(
+				'select'=>'users.id',
+				'condition'=>'users.id=:id',
+				'params'=>array(':id'=>$userId),
+			),
+		))->exists();
   }
  
   // Load user model.
