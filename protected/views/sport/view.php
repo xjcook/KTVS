d22d4c2a69c563064fd4b0cbac630c4792e2bded
mario.racek <mario.racek@gmail.com>
2013-12-10 10:21:33 +0000

    Change Title view in sport/view.php

--- protected/views/sport/view.php
+++ protected/views/sport/view.php
@@ -16,7 +16,7 @@ $this->menu=array(
 );
 ?>
 
-<h1>View Sport #<?php echo $model->id; ?></h1>
+<h1><?php echo $model->name; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
