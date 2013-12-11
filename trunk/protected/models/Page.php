fb1fcca362d446c6281867f4a68ff599c395e7d5
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2013-12-11 10:12:26 +0000

    Add EAdvancedArBehaviour extension to simplify work with M:N relations

--- trunk/protected/models/Page.php
+++ trunk/protected/models/Page.php
@@ -109,4 +109,33 @@ class Page extends CActiveRecord
 	{
 		return parent::model($className);
 	}
+	
+	/**
+	 * Returns a list of behaviors that this model should behave as.
+	 * The return value should be an array of behavior configurations indexed by
+	 * behavior names. Each behavior configuration can be either a string specifying
+	 * the behavior class or an array of the following structure:
+	 * <pre>
+	 * 'behaviorName'=>array(
+	 *     'class'=>'path.to.BehaviorClass',
+	 *     'property1'=>'value1',
+	 *     'property2'=>'value2',
+	 * )
+	 * </pre>
+	 *
+	 * Note, the behavior classes must implement {@link IBehavior} or extend from
+	 * {@link CBehavior}. Behaviors declared in this method will be attached
+	 * to the model when it is instantiated.
+	 *
+	 * For more details about behaviors, see {@link CComponent}.
+	 * @return array the behavior configurations (behavior name=>behavior configuration)
+	 */
+	public function behaviors()
+	{
+		return array(
+			'EAdvancedArBehavior'=>array(
+                'class'=>'application.extensions.EAdvancedArBehavior',
+            ),
+		);
+	}
 }
