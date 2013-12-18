60d7079d0f0e60071444a104b5caa22aa8e43801
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2013-12-18 18:20:40 +0000

    Decode HTML tags

--- trunk/protected/views/page/_view.php
+++ trunk/protected/views/page/_view.php
@@ -4,18 +4,13 @@
 ?>
 
 <div class="view">
-
 	
 	<b><?php echo CHtml::encode($data->getAttributeLabel('title')); ?>:</b>
 	<?php echo CHtml::link(CHtml::encode($data->title),  array('view', 'id'=>$data->id)); ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
-	<?php echo CHtml::encode($data->content); ?>
+	<?php echo CHtml::decode($data->content); ?>
 	<br />
-        <br />
-
-	
-
 
 </div>
