641e5c9f7a8d6e7b9b55ea5008cb24872d28ce68
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2013-12-19 08:36:10 +0000

    Fix migration

--- trunk/protected/migrations/m131219_010019_add_sport_tvobjects.php
+++ trunk/protected/migrations/m131219_010019_add_sport_tvobjects.php
@@ -4,21 +4,21 @@ class m131219_010019_add_sport_tvobjects extends CDbMigration
 {
 	public function safeUp()
 	{
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'1','el_id'=>'1'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'1','tvobject_id'=>'1'));
             
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'2','el_id'=>'1'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'2','tvobject_id'=>'1'));
            
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'3','el_id'=>'1'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'3','tvobject_id'=>'1'));
             
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'4','el_id'=>'1'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'4','tvobject_id'=>'1'));
             
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'5','el_id'=>'1'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'5','tvobject_id'=>'1'));
             
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'6','el_id'=>'2'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'6','tvobject_id'=>'2'));
            
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'7','el_id'=>'2'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'7','tvobject_id'=>'2'));
             
-            $this->insert('tbl_sport_tvobject', array('sport_id'=>'8','el_id'=>'2'));
+            $this->insert('tbl_sport_tvobject', array('sport_id'=>'8','tvobject_id'=>'2'));
             
 	}
 
