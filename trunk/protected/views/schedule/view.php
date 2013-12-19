484d444d78e42ca4f38c88fd49cd518933e1bde8
mario.racek@gmail.com <mario.racek@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2013-12-19 10:16:07 +0000

    Translate schedule/view.php

--- trunk/protected/views/schedule/view.php
+++ trunk/protected/views/schedule/view.php
@@ -3,26 +3,21 @@
 /* @var $model Schedule */
 
 $this->breadcrumbs=array(
-	'Schedules'=>array('index'),
+	'Rozvrhy'=>array('index'),
 	$model->id,
 );
 
 $this->menu=array(
-	array('label'=>'List Schedule', 'url'=>array('index')),
-	array('label'=>'Create Schedule', 'url'=>array('create')),
-	array('label'=>'Update Schedule', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete Schedule', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage Schedule', 'url'=>array('admin')),
+	array('label'=>'Zoznam rozvrhov', 'url'=>array('index')),
+	array('label'=>'Vytvoriť rozvrh', 'url'=>array('create')),
+	array('label'=>'Upraviť rozvrh', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Odstrániť rozvrh', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Spravovať rozvrh', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View Schedule #<?php echo $model->id; ?></h1>
+<h1><?php echo CHtml::encode($model->page->title); ?></h1>
 
-<b><?php echo CHtml::encode($model->page->getAttributeLabel('title')); ?>:</b>
-<?php echo CHtml::encode($model->page->title); ?>
-<br />
-
-<b><?php echo CHtml::encode($model->page->getAttributeLabel('content')); ?>:</b>
 <?php echo CHtml::decode($model->page->content); ?>
 <br />
 
