98dd6b6e43ad21f039934be40d8306edd346fc5d
mario.racek <mario.racek@gmail.com>
2013-12-19 14:32:23 +0000

    Add create subpage

--- protected/views/event/view.php
+++ protected/views/event/view.php
@@ -13,6 +13,8 @@ $this->menu=array(
 	array('label'=>'Upraviť akciu', 'url'=>array('update', 'id'=>$model->id)),
 	array('label'=>'Odstrániť akciu', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
 	array('label'=>'Spravovať akciu', 'url'=>array('admin')),
+	array('label'=>'Vytvoriť podstránku', 'url'=>array('event/createPage')),
+	/*array('label'=>'Upraviť podstránku', 'url'=>array('event/updatePage', 'id'=>$model->id)),*/
 );
 ?>
 
