051b0c92f026c0b8b4534fd3ae8d06d2c2583b3a
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2014-01-16 16:02:19 +0000

    Add class attribute to Schedule

--- trunk/protected/views/schedule/_form.php
+++ trunk/protected/views/schedule/_form.php
@@ -18,7 +18,15 @@
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
 
-	<?php echo $form->errorSummary($scheduleModel, $pageModel); ?>
+	<?php echo $form->errorSummary(array($scheduleModel, $pageModel)); ?>
+	
+	<div class="row">
+		<?php echo $form->labelEx($scheduleModel,'class'); ?>
+		<?php echo $form->dropDownList($scheduleModel,'class', 
+              array('1' => '1.ročník', '2' => '2.ročník', '3' => '3.ročník', '4' => '4.ročník', '5' => '5.ročník'),
+              array('empty' => 'Vyberte ročník')); ?>
+        <?php echo $form->error($scheduleModel,'class'); ?>
+	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($scheduleModel,'tvobject_id'); ?><br/>
