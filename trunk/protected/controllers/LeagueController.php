2464c65fef98614b4be1106d44e237dfd4ef7a8a
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2014-01-18 12:19:00 +0000

    Fix updateSubPage in League & urls

--- trunk/protected/controllers/LeagueController.php
+++ trunk/protected/controllers/LeagueController.php
@@ -322,6 +322,21 @@ class LeagueController extends Controller
 			throw new CHttpException(404,'The requested page does not exist.');
 		return $model;
 	}
+	
+	/**
+	 * Returns the data model based on the primary key given in the GET variable.
+	 * If the data model is not found, an HTTP exception will be raised.
+	 * @param integer $id the ID of the model to be loaded
+	 * @return El the loaded model
+	 * @throws CHttpException
+	 */
+	public function loadPageModel($id)
+	{
+		$model=Page::model()->findByPk($id);
+		if($model===null)
+			throw new CHttpException(404,'The requested page does not exist.');
+		return $model;
+	}
 
 	/**
 	 * Performs the AJAX validation.
