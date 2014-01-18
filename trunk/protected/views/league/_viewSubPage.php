0d2196cdc712ba48c5aade0da50368464771040b
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2014-01-18 11:34:27 +0000

    Fix url error

--- trunk/protected/views/league/_viewSubPage.php
+++ trunk/protected/views/league/_viewSubPage.php
@@ -7,14 +7,14 @@
 	<div id="element">	
    
 	<?php echo CHtml::link(CHtml::encode($data->title), Yii::app()->createUrl('/', array(
-					'event'=>Yii::app()->getRequest()->getParam('id'), 'page'=>$data->id)
+					'league'=>Yii::app()->getRequest()->getParam('id'), 'page'=>$data->id)
 	)); ?>
 	<br />
 	<?php $this->widget('ext.XReadMore.XReadMore', array(
          'showLink'=>true,
          'model'=>$data,
          'linkText'=>'Čítaj viac',
-		 'linkUrl'=>Yii::app()->createUrl('/', array('event'=>Yii::app()->getRequest()->getParam('id'), 'page'=>$data->id)),
+		 'linkUrl'=>Yii::app()->createUrl('/', array('league'=>Yii::app()->getRequest()->getParam('id'), 'page'=>$data->id)),
          'attribute'=>'content',
          'maxChar'=>100,
        ));
