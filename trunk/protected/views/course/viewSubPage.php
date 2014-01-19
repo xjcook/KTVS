4ca4f24e9d7eee5cc3ba7d856bbfabf02bb4b687
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2014-01-19 14:34:40 +0000

    Add missing files

---
+++ trunk/protected/views/course/viewSubPage.php
@@ -0,0 +1,32 @@
+<?php
+/* @var $this PageController */
+/* @var $model Page */
+
+$this->breadcrumbs=array(
+	'Kurzy'=>array('index'),
+	$model->title,
+);
+if(Yii::app()->user->isGuest):
+$this->menu=array(
+	array('label'=>'Zoznam kurzov', 'url'=>array('index')));
+else:
+$this->menu=array(
+	array('label'=>'Zoznam kurzov', 'url'=>array('index')),
+	array('label'=>'Vytvoriť kurz', 'url'=>array('create')),
+	array('label'=>'Upraviť kurz', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Upraviť podstránku kurzu', 'url'=>Yii::app()->user->getUpdateUrl('course',$model->id)),
+	array('label'=>'Odstrániť kurz', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Spravovať kurzy', 'url'=>array('admin')),
+);
+endif;
+?>
+
+<h1><?php echo $model->title; ?></h1>
+
+	<b><?php echo CHtml::encode($model->getAttributeLabel('title')); ?>:</b>
+	<?php echo CHtml::encode($model->title); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($model->getAttributeLabel('content')); ?>:</b>
+	<?php echo CHtml::decode($model->content); ?>
+	<br />
