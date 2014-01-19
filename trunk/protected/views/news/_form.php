76cf13e68d5110c74877acdcec4c66991fe41fa0
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2014-01-19 20:41:11 +0000

    Improve look of the News

--- trunk/protected/views/news/_form.php
+++ trunk/protected/views/news/_form.php
@@ -32,10 +32,27 @@
 		<?php echo $form->error($model,'title'); ?>
 	</div>
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'content'); ?>
-		<?php echo $form->textArea($model,'content',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'content'); ?>
+	<div class="tinymce">
+        <?php echo $form->labelEx($model,'content'); ?><br />
+		<?php $this->widget('application.extensions.tinymce.ETinyMce',
+			array(
+				'model'=>$model,
+                'attribute'=>'content',
+                'editorTemplate'=>'full',
+				'htmlOptions'=>array('rows'=>6, 'cols'=>50, 'class'=>'tinymce'),
+                'options' => array(
+                	'theme_advanced_buttons1' =>
+                    	'undo,redo,|,bold,italic,underline,|,outdent, indent,|,advhr,|,sub,sup,|,bullist,numlist,|,fontsizeselect,',
+                    'theme_advanced_buttons2' => 'tablecontrols,|,cut,copy,paste',
+                    'theme_advanced_buttons3' => '',
+                    'theme_advanced_buttons4' => '',
+                    'theme_advanced_toolbar_location' => 'top',
+                    'theme_advanced_toolbar_align' => 'left',
+                    'theme_advanced_statusbar_location' => 'none',
+                    'theme_advanced_font_sizes' => "10=10pt,11=11pt,12=12pt,13=13pt,14=14pt,15=15pt,16=16pt,17=17pt,18=18pt,19=19pt,20=20pt"
+			)
+		)); ?>            
+		<?php echo $form->error($model,'content'); ?>                                                
 	</div>
 
 	<div class="row">
