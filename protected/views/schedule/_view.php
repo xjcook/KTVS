3b3f6bc9cbfcc048ea4ddbe8eb14f8196ccae9ee
janka.sobkuliak <janka.sobkuliak@gmail.com>
2014-01-19 13:19:58 +0000

    reDesign schedule/_view.php

--- protected/views/schedule/_view.php
+++ protected/views/schedule/_view.php
@@ -4,15 +4,15 @@
 ?>
 
 <div class="view">
-
-<!-- 	<b><?php echo CHtml::encode($data->getAttributeLabel('page_id')); ?>:</b>
-	<?php echo CHtml::link(CHtml::encode($data->page->title), array('view', 'id'=>$data->id)); ?>
-	<br /> -->
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('tvobject_id')); ?>:</b>
-	<?php echo CHtml::encode($data->tvobject->name); ?>
-	<br />
-
-	<?php echo CHtml::decode($data->page->content); ?>
-
+  <div id="element">	
+  <!-- 	<b><?php echo CHtml::encode($data->getAttributeLabel('page_id')); ?>:</b>
+  	<?php echo CHtml::link(CHtml::encode($data->page->title), array('view', 'id'=>$data->id)); ?>
+  	<br /> -->
+  
+  	<b><?php echo CHtml::encode($data->getAttributeLabel('tvobject_id')); ?>:</b>
+  	<?php echo CHtml::encode($data->tvobject->name); ?>
+  	<br />
+  
+  	<?php echo CHtml::decode($data->page->content); ?>
+  </div>
 </div>
