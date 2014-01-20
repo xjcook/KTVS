f76005d515e16779bcdc037602031663378e033b
xjcook@gmail.com <xjcook@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2014-01-20 08:27:00 +0000

    Add _formSubPage into League, Event and Course

--- trunk/protected/views/league/createSubPage.php
+++ trunk/protected/views/league/createSubPage.php
@@ -16,31 +16,7 @@ $this->menu=array(
 
 <h1>Vytvorenie podstránky Ligy</h1>
 
-<div class="form">
-
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'page-form',
-	// Please note: When you enable ajax validation, make sure the corresponding
-	// controller action is handling ajax validation correctly.
-	// There is a call to performAjaxValidation() commented in generated controller code.
-	// See class documentation of CActiveForm for details on this.
-	'enableAjaxValidation'=>false,
+<?php $this->renderPartial('_formSubPage', array(
+	'pageElModel'=>$pageElModel, 
+	'pageModel'=>$pageModel,
 )); ?>
-
-	<?php echo $form->errorSummary(array($pageElModel, $pageModel)); ?>
-
-	<?php echo $form->labelEx($pageElModel,'el_id', array('label'=>'Liga')); ?>
-	<?php echo $form->dropDownList($pageElModel,'el_id',
-			Yii::app()->user->getLeaguesListData(),
-			array('empty'=>'Vyberte ligu')); ?>
-	<?php echo $form->error($pageElModel,'el_id'); ?>
-	
-	<?php $this->widget('application.components.PageWidget', array('model'=>$pageModel,'form'=>$form)); ?>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton($pageElModel->isNewRecord ? 'Vytvoriť' : 'Uložiť'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
-
-</div><!-- form -->
