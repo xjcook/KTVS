c7b08d058fd4f0c4f14957e525c8663b27d86279
janka.sobkuliak@gmail.com <janka.sobkuliak@gmail.com@1181af4b-6a35-841c-2276-d54818eddb8c>
2014-01-20 01:22:15 +0000

    update breadcrumbs

--- trunk/protected/views/event/update.php
+++ trunk/protected/views/event/update.php
@@ -16,6 +16,6 @@ $this->menu=array(
 );
 ?>
 
-<h1>Detail <?php echo $model->name; ?></h1>
+<h1>Úprava <?php echo $model->name; ?></h1>
 
 <?php $this->renderPartial('_form', array('model'=>$model)); ?>
