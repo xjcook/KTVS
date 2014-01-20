03013bc91ee3e772d9e79472f498ef6a455add74
Ondrej Danko <xjcook@users.noreply.github.com>
2014-01-20 08:27:00 +0000

    Add _formSubPage into League, Event and Course

---
+++ protected/views/course/_formSubPage.php
@@ -0,0 +1,34 @@
+<?php
+/* @var $this CourseController */
+/* @var $pageElModel PageEl */
+/* @var $pageModel Page */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'page-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<?php echo $form->errorSummary(array($pageElModel, $pageModel)); ?>
+
+	<?php echo $form->labelEx($pageElModel,'el_id', array('label'=>'Kurz')); ?>
+	<?php echo $form->dropDownList($pageElModel,'el_id',
+			Yii::app()->user->getCoursesListData(),
+			array('empty'=>'Vyberte kurz')); ?>
+	<?php echo $form->error($pageElModel,'el_id'); ?>
+	
+	<?php $this->widget('application.components.PageWidget', array('model'=>$pageModel,'form'=>$form)); ?>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($pageElModel->isNewRecord ? 'Vytvoriť' : 'Uložiť'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
